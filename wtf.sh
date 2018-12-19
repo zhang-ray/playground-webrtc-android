089be80acf65bc49976bb6ef33dcb7e775c714fa
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-19 18:59:15 +0800

    fix tree

--- wtf.sh
+++ wtf.sh
@@ -11,3 +11,44 @@ cd /tmp/depot_tools
 
 ./build/install-build-deps.sh
 
+
+
+
+
+# http://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /tmp/depot_tools
+export PATH=$PATH:/tmp/depot_tools
+
+#tree /tmp/depot_tools # no tree?
+
+cd /tmp/depot_tools
+
+find -name install-build-deps.sh
+
+#./build/install-build-deps.sh
+
+
+
+# https://webrtc.org/native-code/android/
+fetch --nohooks webrtc_android
+gclient sync
+
+
+## compiling phase
+find -name src
+gn gen out/Debug --args='target_os="android" target_cpu="arm"'
+
+
+
+
+ninja -C out/Debug
+
+
+
+
+. build/android/envsetup.sh
+
+
+ninja -C out/Debug AppRTCMobile
+
+
