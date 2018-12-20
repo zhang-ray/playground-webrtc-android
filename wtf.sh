a5b747981779c72f08516c5fd26a26c2be3a16d1
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-12-20 10:38:56 +0800

    [depot] remove something useless

--- wtf.sh
+++ wtf.sh
@@ -1,31 +1,6 @@
-
-
-
-
 # http://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /tmp/depot_tools
-export PATH=$PATH:/tmp/depot_tools
-
-tree /tmp/depot_tools
-cd /tmp/depot_tools
-
-./build/install-build-deps.sh
-
-
-
-
-
-# http://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /tmp/depot_tools
-export PATH=$PATH:/tmp/depot_tools
-
-#tree /tmp/depot_tools # no tree?
-
-cd /tmp/depot_tools
-
-find -name install-build-deps.sh
-
-#./build/install-build-deps.sh
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git 
+export PATH=$PATH:$PWD/depot_tools
 
 
 
